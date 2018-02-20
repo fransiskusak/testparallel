980753129227432d8a97990a4d49221cd3414883
fransiskusak <f.a.k@mail.ugm.ac.id>
2018-02-20 14:11:01 +0700

    @Dunkin ALL TEST PASS semua, assertion kosong dulu

---
+++ src/test/java/com/mycompany/masterserenityjbehave/pages/RegisterPage.java
@@ -0,0 +1,10 @@
+package com.mycompany.masterserenityjbehave.pages;
+
+import net.thucydides.core.pages.PageObject;
+
+/**
+ * Created by Fransiskus A K on 20/02/2018.
+ */
+public class RegisterPage extends PageObject {
+    
+}
