3fc437cf9bfe0241c4236d8f54cd280afbb14da7
fransiskusak <f.a.k@mail.ugm.ac.id>
2018-03-11 21:58:17 +0700

    @Rumah K ALL TEST PASS semua, assertion bisa baik ShouldContainText atau ShouldNotContainText tambahan: add registerPage tambahan lagi: ngisi method registerPage tapi belom selese backuppengamannya file Step Register tambahan lagi: add Invalid!

--- src/test/java/com/mycompany/masterserenityjbehave/AcceptanceTestSuite.java
+++ src/test/java/com/mycompany/masterserenityjbehave/AcceptanceTestSuite.java
@@ -2,4 +2,15 @@ package com.mycompany.masterserenityjbehave;
 
 import net.serenitybdd.jbehave.SerenityStories;
 
-public class AcceptanceTestSuite extends SerenityStories {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class AcceptanceTestSuite extends SerenityStories {
+    @Override
+    public List<String> storyPaths() {
+        List<String> storyPaths = new ArrayList<>();
+        //storyPaths.add( "stories/login/Enroll.story" );
+        storyPaths.add( "stories/Course/Enroll.story" );
+        return storyPaths;
+    }
+}
